9f5f76208d78ea8105892c5ed93217432db1adbb
Nyzuro <marco.strs27@gmail.com>
2025-04-11 15:40:27 +0200

    ✨ F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,3 +1,32 @@
+import { useState } from "react";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+    setInputValue("");
+  };
+
+  return (
+    <div>
+      <h1>Bienvenue chez nous!</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Entrez votre prenom..."
+          value={inputValue}
+          onChange={handleChange}
+          required
+        />
+        <button>Accéder à votre espace</button>
+      </form>
+    </div>
+  );
 }
