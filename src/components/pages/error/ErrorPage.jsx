386489011af569f4cea80293d1570afb27358f36
Nyzuro <marco.strs27@gmail.com>
2025-04-14 14:20:55 +0200

    ♻️ F02: ERROR > change components name

--- src/components/pages/error/ErrorPage.jsx
+++ src/components/pages/error/ErrorPage.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 
-export default function NotFoundPage() {
+export default function ErrorPage() {
   return (
     <div>
-      <h1>NotFoundPage</h1>
+      <h1>ErrorPage</h1>
       <Link to={"/"}>
         <button>Retouner vers la page d'accueil</button>
       </Link>
