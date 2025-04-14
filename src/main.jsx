c01b58bad42d23037b59198120125f60d953f749
Nyzuro <marco.strs27@gmail.com>
2025-04-14 14:19:01 +0200

    ♻️ F02: replace routing system with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App.jsx";
-import NotFoundPage from "./components/pages/notFoundPages/NotFoundPage.jsx";
-import OrderPage from "./components/pages/order/OrderPage.jsx";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
 
-const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/order", element: <OrderPage /> },
-  { path: "*", element: <NotFoundPage /> },
-]);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
