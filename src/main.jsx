761930c42caaef4d61d7a88c3b88d65e7ca3cf6f
Nyzuro <marco.strs27@gmail.com>
2025-04-22 10:48:14 +0200

    🔥 F03: Delete index.css file

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
-import "./index.css";
 import { theme } from "./theme";
 
 createRoot(document.getElementById("root")).render(
