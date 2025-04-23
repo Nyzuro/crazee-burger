c1b55fea90fa29442b484b38cb5f80f6efdc6890
Nyzuro <marco.strs27@gmail.com>
2025-04-23 23:15:34 +0200

    💄 F03: LOGIN > LoginForm  > Add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -17,23 +17,72 @@ export default function LoginForm() {
 
   return (
     <LoginFormStyled action="submit" onSubmit={handleSubmit}>
-      <h1>Bienvenue chez nous!</h1>
-      <br />
-      <h2>Connectez-vous</h2>
-      <input
-        type="text"
-        placeholder="Entrez votre prenom"
-        value={inputValue}
-        onChange={handleChange}
-        required
-      />
-      <button>Accéder à mon espace</button>
+      <div>
+        <h1>Bienvenue chez nous!</h1>
+        <hr />
+        <h2>Connectez-vous</h2>
+      </div>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Entrez votre prenom"
+          value={inputValue}
+          onChange={handleChange}
+          required
+        />
+        <button>Accéder à mon espace</button>
+      </div>
     </LoginFormStyled>
   );
 }
 
 const LoginFormStyled = styled.form`
-  background-color: green;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  text-align: center;
+  padding: 2.5rem 2rem;
+  max-width: 500px;
+  min-width: 400px;
+  font-family: "Amatic SC", sans-serif;
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  hr {
+    border: 1.5px solid #f56a2c;
+    margin-bottom: 40px;
+    margin-top: 32px;
+    width: 400px;
+  }
+
+  h1 {
+    color: white;
+    font-size: 48px;
+  }
+
+  h2 {
+    color: white;
+    font-size: 36px;
+  }
+
+  input {
+    width: 400px;
+    height: 55px;
+    margin: 18px;
+    border-radius: 5px;
+    border-width: 0;
+  }
+
+  button {
+    width: 400px;
+    height: 55px;
+    background-color: #ff9f1b;
+    border-radius: 5px;
+    border-width: 0;
+    color: white;
+  }
 `;
