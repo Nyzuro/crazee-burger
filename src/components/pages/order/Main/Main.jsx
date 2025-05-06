6d13b9186b2bc435649d0b0df20e6ac1855e1b35
Nyzuro <marco.strs27@gmail.com>
2025-05-06 11:22:34 +0200

    🚧 F05: ORDER > MENU > Add grid layout

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -18,6 +18,7 @@ const MainStyled = styled.div`
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  overflow-y: scroll;
 
   display: grid;
   grid-template-columns: 1fr;
--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
 
 export default function Menu() {
-  return <MenuStyled>Menu</MenuStyled>;
+  const [menu, setMenu] = useState(fakeMenu2);
+
+  return (
+    <MenuStyled>
+      {menu.map((product) => {
+        return <div className="product">{product.title}</div>;
+      })}
+    </MenuStyled>
+  );
 }
 
 const MenuStyled = styled.div`
   background-color: lightblue;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-row-gap: 60px;
+  padding: 50px 50px 150px;
+  justify-items: center;
+
+  .product {
+    background-color: red;
+    width: 240px;
+    height: 330px;
+  }
 `;
