5b7bff35ab9e2ea0e76e1561d6e59fdc71f3c4ef
Nyzuro <marco.strs27@gmail.com>
2025-05-15 10:48:01 +0200

    💄 F06: NAVBAR > Right Side > Add spacing

--- src/components/pages/order/Navbar/NavbarRightSide.jsx
+++ src/components/pages/order/Navbar/NavbarRightSide.jsx
@@ -1,22 +1,22 @@
 import styled from "styled-components";
 import { theme } from "../../../../theme";
+import ToggleButton from "../../../reusable-ui/ToggleButton";
 import Profile from "./Profile";
 export default function NavbarRightSide({ username }) {
-  return (
-    <NavbarRightSideStyled>
-      {/* <div className="admin-button">Admin Button</div> */}
-      <Profile username={username} />
-    </NavbarRightSideStyled>
-  );
+	return (
+		<NavbarRightSideStyled>
+			<ToggleButton
+				labelIfUnchecked="ACTIVER LE MODE ADMIN"
+				labelIfChecked="DÉSACTIVER LE MODE ADMIN"
+			/>
+			<Profile username={username} />
+		</NavbarRightSideStyled>
+	);
 }
 
 const NavbarRightSideStyled = styled.div`
-  display: flex;
-  align-items: center;
-  color: ${theme.colors.greyBlue};
-  padding-right: 50px;
-
-  /* .admin-button {
-    background: lightblue;
-  } */
+	display: flex;
+	align-items: center;
+	color: ${theme.colors.greyBlue};
+	padding-right: 50px;
 `;
--- src/components/pages/order/Navbar/Profile.jsx
+++ src/components/pages/order/Navbar/Profile.jsx
@@ -4,60 +4,61 @@ import styled from "styled-components";
 import { theme } from "../../../../theme";
 
 export default function Profile({ username }) {
-  return (
-    <ProfileStyled>
-      <div className="info">
-        <p>
-          Hey, <b>{username}</b>
-        </p>
-        <Link to="/">
-          <div className="description">
-            <small>Se déconnecter</small>
-          </div>
-        </Link>
-      </div>
-      <div className="picture">
-        <BsPersonCircle />
-      </div>
-    </ProfileStyled>
-  );
+	return (
+		<ProfileStyled>
+			<div className="info">
+				<p>
+					Hey, <b>{username}</b>
+				</p>
+				<Link to="/">
+					<div className="description">
+						<small>Se déconnecter</small>
+					</div>
+				</Link>
+			</div>
+			<div className="picture">
+				<BsPersonCircle />
+			</div>
+		</ProfileStyled>
+	);
 }
 
 const ProfileStyled = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  min-width: 100px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	min-width: 100px;
+	padding-left: 50px;
 
-  .info {
-    text-align: right;
-    margin-right: 10px;
-    p {
-      color: ${theme.colors.greyBlue};
-      b {
-        color: ${theme.colors.primary};
-      }
-    }
-  }
+	.info {
+		text-align: right;
+		margin-right: 10px;
+		p {
+			color: ${theme.colors.greyBlue};
+			b {
+				color: ${theme.colors.primary};
+			}
+		}
+	}
 
-  a {
-    text-decoration: none;
-    .description {
-      :hover {
-        text-decoration: underline;
-        color: ${theme.colors.greyDark};
-      }
-      small {
-        color: ${theme.colors.greyBlue};
-        font-size: ${theme.fonts.size.XXS};
-      }
-    }
-  }
+	a {
+		text-decoration: none;
+		.description {
+			:hover {
+				text-decoration: underline;
+				color: ${theme.colors.greyDark};
+			}
+			small {
+				color: ${theme.colors.greyBlue};
+				font-size: ${theme.fonts.size.XXS};
+			}
+		}
+	}
 
-  .picture {
-    font-size: ${theme.fonts.size.P4};
-    display: flex;
-    align-items: center;
-    color: ${theme.colors.greyBlue};
-  }
+	.picture {
+		font-size: ${theme.fonts.size.P4};
+		display: flex;
+		align-items: center;
+		color: ${theme.colors.greyBlue};
+	}
 `;
