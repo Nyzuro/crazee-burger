c5bc3b76c546f7b3c53fd8351cc69aa89dac283b
Nyzuro <marco.strs27@gmail.com>
2025-05-19 23:19:06 +0200

    ✨ F07: ORDER > AdminPanel > Show panel only in admin mode

--- src/components/pages/order/Main/AdminPanel.jsx
+++ src/components/pages/order/Main/AdminPanel.jsx
@@ -1,41 +1,55 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import { FiChevronDown } from "react-icons/fi";
 import { MdModeEditOutline } from "react-icons/md";
 import styled from "styled-components";
+import OrderContext from "../../../../context/OrderContext";
 import { theme } from "../../../../theme";
 
 export default function AdminPanel() {
 	const [activePanel, setActivePanel] = useState("add");
+	const { isModeAdmin } = useContext(OrderContext);
 
 	return (
-		<AdminPanelStyled>
-			<div className="navigation">
-				<button className="white-theme">
-					<FiChevronDown />
-				</button>
-				<button
-					className={activePanel === "add" ? "dark-theme" : "white-theme"}
-					onClick={() => setActivePanel("add")}
-				>
-					<AiOutlinePlus />
-					Ajouter un produit
-				</button>
-				<button
-					className={activePanel === "edit" ? "dark-theme" : "white-theme"}
-					onClick={() => setActivePanel("edit")}
-				>
-					<MdModeEditOutline />
-					Modifier un produit
-				</button>
-			</div>
-			<div className="panel">
-				{activePanel === "add" ? "Ajouter un produit" : "Modifier un produit"}
-			</div>
-		</AdminPanelStyled>
+		<>
+			{isModeAdmin && (
+				<AdminPanelStyled>
+					<div className="navigation">
+						<button className="white-theme">
+							<FiChevronDown />
+						</button>
+
+						<button
+							className={
+								activePanel === "add" ? "dark-theme" : "white-theme"
+							}
+							onClick={() => setActivePanel("add")}
+						>
+							<AiOutlinePlus />
+							Ajouter un produit
+						</button>
+
+						<button
+							className={
+								activePanel === "edit" ? "dark-theme" : "white-theme"
+							}
+							onClick={() => setActivePanel("edit")}
+						>
+							<MdModeEditOutline />
+							Modifier un produit
+						</button>
+					</div>
+
+					<div className="panel">
+						{activePanel === "add"
+							? "Ajouter un produit"
+							: "Modifier un produit"}
+					</div>
+				</AdminPanelStyled>
+			)}
+		</>
 	);
 }
-
 const AdminPanelStyled = styled.div`
 	position: absolute;
 	width: 100%;
