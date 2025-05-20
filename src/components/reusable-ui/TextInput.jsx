ef6abb66b280949fa3db59480b803856133faaae
Nyzuro <marco.strs27@gmail.com>
2025-05-20 12:39:06 +0200

    ♻️ Update font-size

--- src/components/reusable-ui/TextInput.jsx
+++ src/components/reusable-ui/TextInput.jsx
@@ -2,41 +2,41 @@ import styled from "styled-components";
 import { theme } from "../../theme";
 
 export default function TextInput({ value, onChange, Icon, ...extraProps }) {
-  return (
-    <InputStyled>
-      {Icon && Icon}
-      <input type="text" value={value} onChange={onChange} {...extraProps} />
-    </InputStyled>
-  );
+	return (
+		<InputStyled>
+			{Icon && Icon}
+			<input type="text" value={value} onChange={onChange} {...extraProps} />
+		</InputStyled>
+	);
 }
 
 const InputStyled = styled.div`
-  background-color: #fff;
-  margin: 18px 0px;
-  padding: 18px 24px;
-  border-radius: ${theme.borderRadius.round};
-  display: flex;
-  align-items: center;
+	background-color: #fff;
+	margin: 18px 0px;
+	padding: 18px 24px;
+	border-radius: ${theme.borderRadius.round};
+	display: flex;
+	align-items: center;
 
-  .input-icon {
-    font-size: ${theme.fonts.size.P0};
-    margin-right: 8px;
-    color: ${theme.colors.greySemiDark};
-  }
+	.input-icon {
+		font-size: ${theme.fonts.size.SM};
+		margin-right: 8px;
+		color: ${theme.colors.greySemiDark};
+	}
 
-  input {
-    border: none;
-    font-size: ${theme.fonts.size.P0};
-    color: ${theme.colors.dark};
-    width: 100%;
+	input {
+		border: none;
+		font-size: ${theme.fonts.size.SM};
+		color: ${theme.colors.dark};
+		width: 100%;
 
-    &::placeholder {
-      background: ${theme.colors.white};
-      color: ${theme.colors.greyMedium};
-    }
+		&::placeholder {
+			background: ${theme.colors.white};
+			color: ${theme.colors.greyMedium};
+		}
 
-    &:focus-visible {
-      outline: none;
-    }
-  }
+		&:focus-visible {
+			outline: none;
+		}
+	}
 `;
